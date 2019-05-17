2738834307d7e982777697d782c6df2a7aa3c7b6
JoaquinFernandezC <jafernandez1@miuandes.cl>
2019-05-17 15:07:09 -0400

    Created sub-dataset for testing

--- Taller2.R
+++ Taller2.R
@@ -20,19 +20,40 @@ names(data_set) <- c("review_id", "user_id", "business_id", "stars",
                      "text", "useful", "funny", "cool")
 
 head(data_set)
-# Load comment to Corpus
+
+#Create sub-dataset for better testing
+
+sub_data_set <- subset(data_set, business_id == "iCQpiavjjPzJ5_3gPD5Ebg")
+
+# Load text to Corpus
 
 docs <- Corpus(VectorSource(data_set$text))
+sub_docs <- Corpus(VectorSource(sub_data_set$text))
+
+inspect(sub_docs)
 
 # Clean special characters(punctiation, extra space, numbers, etc.)
 
 toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+
 docs <- tm_map(docs, removePunctuation)
 docs <- tm_map(docs, removeNumbers)
-docs <- tm_map(docs, stripWhitespace)
 docs <- tm_map(docs, removeWords, stopwords("english"))
 docs <- tm_map(docs, toSpace, "/")
 docs <- tm_map(docs, toSpace, "@")
 docs <- tm_map(docs, toSpace, "-")
+docs <- tm_map(docs, stripWhitespace)
+
+# sub_docs
+
+sub_docs <- tm_map(sub_docs, removePunctuation)
+sub_docs <- tm_map(sub_docs, removeNumbers)
+sub_docs <- tm_map(sub_docs, removeWords, stopwords("english"))
+sub_docs <- tm_map(sub_docs, toSpace, "/")
+sub_docs <- tm_map(sub_docs, toSpace, "@")
+sub_docs <- tm_map(sub_docs, toSpace, "-")
+sub_docs <- tm_map(sub_docs, stripWhitespace)
+
+inspect(sub_docs)
 
 
