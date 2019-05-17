8caa00b5b3355a3e1fb4c7321bfe3958eb907fa9
JoaquinFernandezC <jafernandez1@miuandes.cl>
2019-05-17 14:55:36 -0400

    Created corpus and cleaned special characters

--- Taller2.R
+++ Taller2.R
@@ -1,4 +1,4 @@
-#First run, uncomment all install.packages, select them all and execute them
+# First run, uncomment all install.packages, select them all and execute them
 
 # install.packages("tm")
 # install.packages("SnowballC")
@@ -18,4 +18,21 @@ data_set <- read.csv("yelp_reviews_big.csv", header = FALSE)
 
 names(data_set) <- c("review_id", "user_id", "business_id", "stars",
                      "text", "useful", "funny", "cool")
+
+head(data_set)
+# Load comment to Corpus
+
+docs <- Corpus(VectorSource(data_set$text))
+
+# Clean special characters(punctiation, extra space, numbers, etc.)
+
+toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, removeNumbers)
+docs <- tm_map(docs, stripWhitespace)
+docs <- tm_map(docs, removeWords, stopwords("english"))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "-")
+
 
