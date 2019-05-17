fe2888b1e93025c1e5035459f3699c04ad210197
JoaquinFernandezC <jafernandez1@miuandes.cl>
2019-05-17 16:01:31 -0400

    Created word matrix and cloud

--- Taller2.R
+++ Taller2.R
@@ -56,4 +56,18 @@ sub_docs <- tm_map(sub_docs, stripWhitespace)
 
 inspect(sub_docs)
 
+# Word matrix
 
+dtm <- TermDocumentMatrix(sub_docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m), decreasing = TRUE)
+d <- data.frame(word = names(v), freq=v)
+
+head(d, 10)
+
+# Word cloud
+
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words = 200, random.order = FALSE, rot.per = 0.35,
+          colors=brewer.pal(8, "Dark2"))
